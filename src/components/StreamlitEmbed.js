6bf3cf692eabc2ed0e555a78a34007e910a31b71
pietrolo94 <pietro.zoffoli1994@gmail.com>
2024-11-27 23:32:52 +0100

    Title Update

--- src/components/StreamlitEmbed.js
+++ src/components/StreamlitEmbed.js
@@ -2,28 +2,29 @@ import React from "react";
 
 function StreamlitEmbed() {
   return (
-    <div style={{ width: '100%', height: '100%', padding: '20px', marginTop: '80px' }}> 
-
-      <h2 style={{ 
-        fontSize: '2rem', 
-        fontWeight: 'bold', 
-        color: '#333', 
-        textAlign: 'center', 
-        marginBottom: '20px' 
-      }}>
-        Streamlit Iris Classification App
-      </h2>
+    <div className="w-full h-full p-5 mt-10">
+        <h2
+          style={{
+            textAlign: 'center',
+            fontSize: '2rem', 
+            fontWeight: '600', 
+            color: '#4C51BF'
+          }}
+        >
+          Streamlit Iris Classification App
+        </h2>
 
       <iframe
         src="https://irislogisticregression.streamlit.app/?embed_options=dark_theme,light_theme&embedded=true"  
         width="100%"
         height="800"
         title="Streamlit App"
-        style={{ borderRadius: '10px', border: 'none', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}
+        className="rounded-lg border-none shadow-xl"
       ></iframe>
     </div>
   );
 }
 
 export default StreamlitEmbed;
+
 
